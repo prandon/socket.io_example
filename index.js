48b9e224009a2d82c8a2ec0ff9b3e4b42e03a499
Pranjal Nartam <Pranjal.Nartam@capco.com>
2019-03-06 17:14:00 +0530

    configured client

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
+  console.log('user connected');
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
